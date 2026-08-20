944521018506145284ee6457be95f41a1f7ce870
ashikurrahman88722-code <ashikurrahman88722@gmail.com>
2026-08-20 09:15:59 +0600

    some modification in comment shwoing section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -302,7 +302,7 @@ value={editing === comment._id ? editComment : comment.comment || ""}
     <button
       onClick={() => handleEdit(comment._id)}
       className="mt-10 cursor-pointer">
-      <Send className={comment.comment ? "text-gray-400" :"text-blue-500"} />
+<Send className={comment.comment ? "text-gray-400" : editComment.trim() ? "text-blue-500" : "text-gray-400"} />
 
     </button>
   )}
