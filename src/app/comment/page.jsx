076fbcb8fb3ee9ff6c40a7c199aea986de15fe11
ashikurrahman88722-code <ashikurrahman88722@gmail.com>
2026-08-20 08:03:47 +0600

    disabled filed added in comment box

--- src/app/comment/page.jsx
+++ src/app/comment/page.jsx
@@ -71,6 +71,7 @@ const CommentPage = () => {
 
       <button
         type="submit"
+        disabled={!comment}
         className="absolute right-3 cursor-pointer text-blue-500 hover:text-blue-600 transition p-1"
         aria-label="Send comment"
       >
